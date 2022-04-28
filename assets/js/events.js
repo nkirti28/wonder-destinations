bf2a5da616b9dfa325ef205b3b8bac27fa1d5b67
carlosrrdev <rr.carlos.dev@gmail.com>
2022-04-28 11:36:05 -0500

    Initial layout

---
+++ assets/js/events.js
@@ -0,0 +1,7 @@
+// fetch(
+//   "https://api.seatgeek.com/2/events?&geoip=true&client_id=MjY3Mzk1NjZ8MTY1MTAxNzc0NS41MDA4MjU2"
+// ).then(function (response) {
+//   response.json().then(function (data) {
+//     console.log(data);
+//   });
+// });
